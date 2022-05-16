a1686da3a7351473cfe1de964d55d58627e544fe
Zean23 <100299651+Zean23@users.noreply.github.com>
2022-05-16 13:21:17 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -5,12 +5,17 @@ function walking_sign2 () {
     range.showColor(neopixel.colors(NeoPixelColors.Black))
     range = strip.range(2, 1)
     range.showColor(neopixel.colors(NeoPixelColors.Green))
-    range = strip.range(3, 1)
-    range.showColor(neopixel.colors(NeoPixelColors.Green))
-    basic.showIcon(IconNames.StickFigure)
-    for (let index = 0; index <= 15; index++) {
-        basic.showNumber(15 - index)
+    if (songs < 1) {
+        music.stopAllSounds()
+    } else {
+        countdown = 15
+        for (let index = 0; index < 15; index++) {
+            countdown += -1
+            music.playTone(262, music.beat(BeatFraction.Half))
+            basic.showNumber(countdown)
+        }
     }
+    basic.clearScreen()
 }
 function green () {
     range = strip.range(0, 1)
@@ -19,8 +24,6 @@ function green () {
     range.showColor(neopixel.colors(NeoPixelColors.Black))
     range = strip.range(2, 1)
     range.showColor(neopixel.colors(NeoPixelColors.Green))
-    range = strip.range(3, 1)
-    range.showColor(neopixel.colors(NeoPixelColors.Black))
 }
 input.onButtonPressed(Button.A, function () {
     walking_sign()
@@ -35,11 +38,12 @@ function walking_sign () {
     range = strip.range(3, 1)
     range.showColor(neopixel.colors(NeoPixelColors.Green))
     basic.showIcon(IconNames.StickFigure)
-    for (let index = 0; index <= 15; index++) {
-        basic.showNumber(15 - index)
+    for (let countdown = 0; countdown <= 15; countdown++) {
+        basic.showNumber(15 - countdown)
     }
 }
 input.onButtonPressed(Button.B, function () {
+    songs = 1
     walking_sign2()
 })
 function yellow () {
@@ -49,8 +53,6 @@ function yellow () {
     range.showColor(neopixel.colors(NeoPixelColors.Yellow))
     range = strip.range(2, 1)
     range.showColor(neopixel.colors(NeoPixelColors.Black))
-    range = strip.range(3, 1)
-    range.showColor(neopixel.colors(NeoPixelColors.Black))
 }
 function red () {
     range = strip.range(0, 1)
@@ -59,13 +61,13 @@ function red () {
     range.showColor(neopixel.colors(NeoPixelColors.Black))
     range = strip.range(2, 1)
     range.showColor(neopixel.colors(NeoPixelColors.Black))
-    range = strip.range(3, 1)
-    range.showColor(neopixel.colors(NeoPixelColors.Black))
 }
+let countdown = 0
+let songs = 0
 let range: neopixel.Strip = null
 let strip: neopixel.Strip = null
 basic.showIcon(IconNames.Heart)
-strip = neopixel.create(DigitalPin.P16, 4, NeoPixelMode.RGB)
+strip = neopixel.create(DigitalPin.P0, 4, NeoPixelMode.RGB)
 strip.setBrightness(30)
 basic.forever(function () {
     green()
